42ebade2afc3426a43274f652f021deaec487ceb
brewost <156411525+brewost@users.noreply.github.com>
2024-06-24 17:02:20 +0200

    astro tut finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+site: "https://example.com";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact()]
+});
